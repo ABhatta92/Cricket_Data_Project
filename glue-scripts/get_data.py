ae7c1995d1cb6613c4747bdab8749d5c7ce9e684
ABhatta92 <abhiroop2792@gmail.com>
2024-11-02 21:35:12 +0530

    updated code for glue job

--- glue-scripts/get_data.py
+++ glue-scripts/get_data.py
@@ -3,6 +3,12 @@ import requests
 import zipfile
 import io
 import os
+import boto3
+
+bucket_name = 'cricket-bucket-obi'
+output_folder = 'raw_cricsheet_data'
+
+s3_client = boto3.client('s3')
 
 # Collecting json data from cricsheet
 url = 'https://cricsheet.org/downloads/t20s_json.zip'
@@ -15,6 +21,8 @@ response.raise_for_status()
 
 # Extracting all data into data folder
 with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
-    zip_ref.extractall(output_folder)
+    for file_name in zip_ref.namelist():
+        with zip_ref.open(file_name) as extracted_file:
+            s3_client.upload_fileobj(extracted_file, bucket_name, f'{output_folder}{file_name}')
 
 print(f'Files extracted to {output_folder}')
