9324ea72afa9b36d663bf5c506d76a07c3bc3bca
ABhatta92 <abhiroop2792@gmail.com>
2024-11-03 21:10:58 +0530

    latest

--- scripts/common_functions.py
+++ scripts/common_functions.py
@@ -14,8 +14,20 @@ sys.path.insert(0, root_dir)
 def create_spark_session(name):
     return SparkSession.builder.appName(name).getOrCreate()
 
-def create_spark_session_with_s3(name):
-    spark = 
+def create_spark_session_with_s3(name, access_key, secret_key):
+    spark = SparkSession.builder \
+        .appName(name) \
+        .config("spark.hadoop.fs.s3a.access.key", access_key) \
+        .config("spark.hadoop.fs.s3a.secret.key", secret_key) \
+        .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+        .config("spark.hadoop.fs.s3a.connection.maximum", "100") \
+        .config("spark.hadoop.fs.s3a.attempts.maximum", "10") \
+        .config("spark.hadoop.fs.s3a.paging.enabled", "true") \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.2.0,com.amazonaws:aws-java-sdk-bundle:1.11.375") \
+        .getOrCreate()
+    
+    return spark
 
 
 
